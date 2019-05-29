f220f69c1dd329776a98e72c882063bb2215c376
Zain Arshad <>
2019-05-29 16:08:18 +0500

    Remove POD directory

--- Pods/Target Support Files/Pods-Shimmer-POCTests/Pods-Shimmer-POCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shimmer_POCTests : NSObject
-@end
-@implementation PodsDummy_Pods_Shimmer_POCTests
-@end
--- Pods/Target Support Files/Pods-Shimmer-POCUITests/Pods-Shimmer-POCUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Shimmer_POCUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Shimmer_POCUITests
-@end
